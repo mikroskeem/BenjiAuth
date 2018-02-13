45ab983442596d605072a14beddb3c8592d8e442
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-02-14 00:14:20 +0200

    Expose reload (temporarily)

--- src/main/kotlin/eu/mikroskeem/benjiauth/commands/admin/BenjiAuthCommand.kt
+++ src/main/kotlin/eu/mikroskeem/benjiauth/commands/admin/BenjiAuthCommand.kt
@@ -8,14 +8,28 @@ package eu.mikroskeem.benjiauth.commands.admin
 
 import eu.mikroskeem.benjiauth.COMMAND_BENJIAUTH
 import eu.mikroskeem.benjiauth.authMessage
+import eu.mikroskeem.benjiauth.plugin
 import net.md_5.bungee.api.CommandSender
 import net.md_5.bungee.api.plugin.Command
+import net.md_5.bungee.api.plugin.TabExecutor
 
 /**
  * @author Mark Vainomaa
  */
-class BenjiAuthCommand: Command("benjiauth", COMMAND_BENJIAUTH) {
+class BenjiAuthCommand: Command("benjiauth", COMMAND_BENJIAUTH), TabExecutor {
     override fun execute(sender: CommandSender, args: Array<out String>) {
         sender.authMessage("Not done yet, annoy @mikroskeem with it")
+
+        // TODO
+        if(args.isNotEmpty() && args[0].equals("reload", ignoreCase = true)) {
+            plugin.reloadConfig()
+        }
+    }
+
+    override fun onTabComplete(sender: CommandSender, args: Array<out String>): Iterable<String> {
+        return when {
+            args.size <= 1 -> listOf("reload", "unregister")
+            else -> emptyList()
+        }
     }
 }
