1cdf54cc09bd6aa83f2f3239193aa5392a3467d2
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-02-14 00:14:32 +0200

    Add license headers

--- src/main/kotlin/eu/mikroskeem/benjiauth/config/messages/LoginMessages.kt
+++ src/main/kotlin/eu/mikroskeem/benjiauth/config/messages/LoginMessages.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018 Mark Vainomaa
+ *
+ * This source code is proprietary software and must not be distributed and/or copied without the express permission of Mark Vainomaa
+ */
+
 package eu.mikroskeem.benjiauth.config.messages
 
 import ninja.leaping.configurate.objectmapping.Setting
--- src/main/kotlin/eu/mikroskeem/benjiauth/tasks/Task.kt
+++ src/main/kotlin/eu/mikroskeem/benjiauth/tasks/Task.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018 Mark Vainomaa
+ *
+ * This source code is proprietary software and must not be distributed and/or copied without the express permission of Mark Vainomaa
+ */
+
 package eu.mikroskeem.benjiauth.tasks
 
 import java.util.concurrent.TimeUnit
